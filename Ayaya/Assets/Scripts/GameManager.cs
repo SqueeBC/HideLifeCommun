879b0460160a9382917eefeb793d55a3a7e25513
SqueeBC <etiennesamatiallian@gmail.com>
2019-05-31 19:20:08 +0200

    Revert "test"
    This reverts commit a27f3b3831278c5c94d571b3f08054c0df983e2c.

--- Ayaya/Assets/Scripts/GameManager.cs
+++ Ayaya/Assets/Scripts/GameManager.cs
@@ -1,65 +1,53 @@
 using System.Collections.Generic;
 using trucs_perso;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
 public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text WinText;
+    private float time = 600;
+    private static Dictionary<string, Player> players = new Dictionary<string, Player>();
+
+    public void RegisterPlayer(string netID , Player player)//l'id du joueur selon le serv     
     {
-        [SerializeField]
-        private Text WinText;
-        private float time = 600;
-        private static Dictionary<string, Player> players = new Dictionary<string, Player>();
+        string playerID = "Player " + netID;
+        players.Add(playerID, player);          
+    }
 
-        public void RegisterPlayer(string netID , Player player)//l'id du joueur selon le serv     
+    public void VictoryForProps()
+    {
+        if (PlayerPrefs.GetString("language", "english") == "english")
+            WinText.text = "Props have won !";
+        else
         {
-            string playerID = "Player " + netID;
-            players.Add(playerID, player);          
+            WinText.text = "Les Props ont gagné !";
         }
-<<<<<<< HEAD
-=======
-        public void UnRegisterPlayer(string playerID)
-        {
-            players.Remove(playerID);
-        }
-
-        public void VictoryForProps()
-        {
-            if (PlayerPrefs.GetString("language", "english") == "english")
-                WinText.text = "Props have won !";
-            else
-            {
-                WinText.text = "Les Props ont gagné !";
-            }
->>>>>>> parent of 570c2388... azert
           
-                foreach (Prop prop in players.Values)
-                {
-                    prop.victory++;
-                }
-        }
-        public void VictoryForHunters()
+        foreach (Prop prop in players.Values)
         {
-<<<<<<< HEAD
+            prop.victory++;
+        }
+    }
+    public void VictoryForHunters()
+    {
+        if (PlayerPrefs.GetString("language", "english") == "english")
+            WinText.text = "Hunters have won !";
+        else
+        {
             WinText.text = "Les Chasseurs ont gagné !";
         }
-=======
-            if (PlayerPrefs.GetString("language", "english") == "english")
-                WinText.text = "Hunters have won !";
-            else
-            {
-                WinText.text = "Les Chasseurs ont gagné !";
-            }
->>>>>>> parent of 570c2388... azert
           
-            foreach (Hunter hunter in players.Values)
-            {
-                hunter.victory++;
-            }
+        foreach (Hunter hunter in players.Values)
+        {
+            hunter.victory++;
         }
+    }
 
-<<<<<<< HEAD
     public void UnRegisterPlayer(string  playerID)
     {
         players.Remove(playerID);
@@ -76,67 +64,75 @@ public class GameManager : MonoBehaviour
         GUILayout.BeginArea(new Rect(200, 200, 200, 200));
         GUILayout.BeginVertical();
         foreach (string playerID in players.Keys)
-=======
-        public static Player GetPlayer(string playerID)
->>>>>>> parent of 570c2388... azert
         {
-            return players[playerID];
+            GUILayout.Label(playerID + "-"+players[playerID].transform.name);
         }
 
-        private void OnGUI() //permet d'afficher les joueurs
-        {
-            int Min;
-            GUILayout.BeginArea(new Rect(200, 200, 200, 200));
-                GUILayout.BeginVertical();
-                foreach (string playerID in players.Keys)
-                {
-                       GUILayout.Label(playerID + "-"+players[playerID].transform.name);
-                }
+        Min = Mathf.RoundToInt((time / 60 - 0.5f));
+        GUILayout.Label("-Time :" + Mathf.RoundToInt((Min )) +  "min "+Mathf.RoundToInt(time-Min*60)+" s");
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+    }
 
-                Min = Mathf.RoundToInt((time / 60 - 0.5f));
-                GUILayout.Label("-Time :" + Mathf.RoundToInt((Min )) +  "min "+Mathf.RoundToInt(time-Min*60)+" s");
-              GUILayout.EndVertical();
-                GUILayout.EndArea();
-        }
-
-        private void Update()
+    private void Update()
+    {
+        foreach (var player in FindObjectsOfType<Player>())
         {
-<<<<<<< HEAD
-            if (time <= 0)
-=======
-            foreach (var player in FindObjectsOfType<Player>())
+
+            if (!players.ContainsKey("Player " + player.id) && player != null)
             {
-                if (player != null && !players.ContainsKey("Player " + player.id))
+
+                RegisterPlayer(player.id, player);
+
+                if (player.gameObject.GetComponent<Hunter>() == null && player.gameObject.GetComponent<Prop>() == null)
                 {
-                    Debug.Log(player.name);
-                    RegisterPlayer(player.id, player);
-                    if(player.gameObject.GetComponent<Hunter>()==null && player.gameObject.GetComponent<Prop>()==null && SceneManager.GetActiveScene().buildIndex != 6)                   
+                    if (SceneManager.GetActiveScene().buildIndex != 6)
+                    {
                         AssignRole(player);
+                        Debug.Log("non");
+                    }
+                   
+                    else
+                    { Debug.Log("oui");
+                        if (player.gameObject.GetComponent<NetworkIdentity>().isLocalPlayer) //pour le tuto
+                        {
+                            
+                            Debug.Log(player.name);
+                            UnRegisterPlayer("Player " + player.id);
+                            Destroy(player);
+                            player.gameObject.AddComponent<Hunter>();
+                            player.gameObject.GetComponent<Hunter>().id = player.id;
+                        }
+
+                    }
+
+
                 }
             }
-            if(time<=0)
->>>>>>> parent of 570c2388... azert
+
+
+        }
+
+        if (SceneManager.GetActiveScene().buildIndex != 6)
+        {
+            if (time <= 0)
                 VictoryForProps();
             bool Mybool = true;
             foreach (Player player in players.Values)
             {
                 Mybool = Mybool && player.gameObject.GetComponent<Prop>() == null;
             }
-<<<<<<< HEAD
 
             if (Mybool)
-=======
-            if(Mybool)
->>>>>>> parent of 570c2388... azert
                 VictoryForHunters();
             time -= Time.deltaTime;
 
         }
+    }
 
-        private void AssignRole(Player player)
-        {
+    private void AssignRole(Player player)
+    {
 
-<<<<<<< HEAD
         UnRegisterPlayer("Player "+player.id);
         Destroy(player);    
             
@@ -165,28 +161,5 @@ public class GameManager : MonoBehaviour
         }
          
             
-=======
-            UnRegisterPlayer("Player "+player.id);
-            Destroy(player);
-        
-            int nbrhuntertot = 0;
-            int nbrhunter = Mathf.RoundToInt(players.Count * 3 / 10+1);
-            foreach (Player _player in players.Values)
-            {
-                if (_player.GetComponent<Hunter>() != null)
-                    nbrhuntertot++;                  
-            }
-            if (nbrhuntertot < nbrhunter)
-            {
-                player.gameObject.AddComponent<Hunter>();
-                player.gameObject.GetComponent<Hunter>().id = player.id;
-            }
-            else
-            {
-                player.gameObject.AddComponent<Prop>();
-                player.gameObject.GetComponent<Prop>().id = player.id;
-            }
-        }
->>>>>>> parent of 570c2388... azert
     }
-    
+}
