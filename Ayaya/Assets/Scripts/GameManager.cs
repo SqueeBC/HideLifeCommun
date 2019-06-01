b73d1027e27988234f4966565b2d055dbdbb4f7f
FIrewop1 <gabriel.lecherf@epita.fr>
2019-06-01 19:09:33 +0200

    Network

--- Ayaya/Assets/Scripts/GameManager.cs
+++ Ayaya/Assets/Scripts/GameManager.cs
@@ -6,17 +6,17 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
-    [SerializeField]
-    private Text WinText;
+    [SerializeField] private Text WinText;
     private float time = 600;
     private static Dictionary<string, Player> players = new Dictionary<string, Player>();
+    private bool start;
 
-    public void RegisterPlayer(string netID , Player player)//l'id du joueur selon le serv     
+    public void RegisterPlayer(string netID, Player player) //l'id du joueur selon le serv     
     {
         string playerID = "Player " + netID;
-        players.Add(playerID, player);          
+        players.Add(playerID, player);
     }
 
     public void VictoryForProps()
@@ -27,12 +27,13 @@ public class GameManager : MonoBehaviour
         {
             WinText.text = "Les Props ont gagné !";
         }
-          
+
         foreach (Prop prop in players.Values)
         {
             prop.victory++;
         }
     }
+
     public void VictoryForHunters()
     {
         if (PlayerPrefs.GetString("language", "english") == "english")
@@ -41,14 +42,14 @@ public class GameManager : MonoBehaviour
         {
             WinText.text = "Les Chasseurs ont gagné !";
         }
-          
+
         foreach (Hunter hunter in players.Values)
         {
             hunter.victory++;
         }
     }
 
-    public void UnRegisterPlayer(string  playerID)
+    public void UnRegisterPlayer(string playerID)
     {
         players.Remove(playerID);
     }
@@ -61,24 +62,26 @@ public class GameManager : MonoBehaviour
     private void OnGUI() //permet d'afficher les joueurs
     {
         string spect = "dead";
-        GUIStyle style = new GUIStyle();//pour changer la police du texte
+        GUIStyle style = new GUIStyle(); //pour changer la police du texte
         int Min;
         GUILayout.BeginArea(new Rect(200, 200, 200, 200));
         GUILayout.BeginVertical();
+        start = GUILayout.Button("Ready");
         foreach (string playerID in players.Keys)
         {
-            GUILayout.Label(playerID + "-"+players[playerID].transform.name,style);
+            GUILayout.Label(playerID + "-" + players[playerID].transform.name, style);
         }
+
         style.fontSize = 24;
         Min = Mathf.RoundToInt((time / 60 - 0.5f));
-        GUILayout.Label("-Time :" + Mathf.RoundToInt((Min )) +  "min "+Mathf.RoundToInt(time-Min*60)+" s",style);
+        GUILayout.Label("-Time :" + Mathf.RoundToInt((Min)) + "min " + Mathf.RoundToInt(time - Min * 60) + " s", style);
         GUILayout.EndVertical();
         GUILayout.EndArea();
-     
+
         style.fontSize = 14;
         GUILayout.BeginArea(new Rect(950, 200, 200, 200));
         GUILayout.BeginVertical();
-        GUILayout.Label("Prop :",style);
+        GUILayout.Label("Prop :", style);
         style.fontSize = 20;
         foreach (Player prop in players.Values)
         {
@@ -86,24 +89,26 @@ public class GameManager : MonoBehaviour
             {
                 if (prop.CompareTag("Spectator"))
                 {
-                
-                    GUILayout.Label("-DEAD " + prop.transform.name, style);}
+
+                    GUILayout.Label("-DEAD " + prop.transform.name, style);
+                }
                 else
                 {
                     GUILayout.Label("-" + prop.transform.name, style);
                 }
             }
         }
+
         GUILayout.EndVertical();
         GUILayout.EndArea();
         GUILayout.BeginArea(new Rect(750, 200, 200, 200));
         GUILayout.BeginVertical();
         style.fontSize = 14;
-        GUILayout.Label("Hunter :",style);
+        GUILayout.Label("Hunter :", style);
         style.fontSize = 20;
         foreach (Player hunter in players.Values)
         {
-            if (hunter.GetComponent<Hunter>() != null)    
+            if (hunter.GetComponent<Hunter>() != null)
             {
                 if (hunter.CompareTag("Spectator"))
                     GUILayout.Label("-DEAD " + hunter.transform.name, style);
@@ -113,9 +118,10 @@ public class GameManager : MonoBehaviour
                 }
             }
         }
+
         GUILayout.EndVertical();
         GUILayout.EndArea();
-    
+
     }
 
     private void Update()
@@ -127,38 +133,41 @@ public class GameManager : MonoBehaviour
             {
 
                 RegisterPlayer(player.id, player);
+            }
 
-                if (player.gameObject.GetComponent<Hunter>() == null && player.gameObject.GetComponent<Prop>() == null)
+            if (player.gameObject.GetComponent<Hunter>() == null && player.gameObject.GetComponent<Prop>() == null)
+            {
+                if (SceneManager.GetActiveScene().buildIndex != 6)
                 {
-                    if (SceneManager.GetActiveScene().buildIndex != 6)
-                    {
-                        AssignRole(player);
-                        
-                       
-                    }
                    
-                    else
-                    { 
-                        if (player.gameObject.GetComponent<NetworkIdentity>().isLocalPlayer) //pour le tuto
-                        {
-                            
-                            
-                            UnRegisterPlayer("Player " + player.id);
-                            Destroy(player);
-                            player.gameObject.AddComponent<Hunter>();
-                            player.gameObject.GetComponent<Hunter>().id = player.id;
-                        }
-
-                    }
+                        AssignRole(player);
 
 
                 }
+
+                else
+                {
+                    if (player.gameObject.GetComponent<NetworkIdentity>().isLocalPlayer) //pour le tuto
+                    {
+
+
+                        UnRegisterPlayer("Player " + player.id);
+                        Destroy(player);
+                        player.gameObject.AddComponent<Hunter>();
+                        player.gameObject.GetComponent<Hunter>().id = player.id;
+                    }
+
+                }
+
+
             }
-
-
         }
 
-        if (SceneManager.GetActiveScene().buildIndex != 6)
+
+    
+
+
+if (SceneManager.GetActiveScene().buildIndex != 6)
         {
             if (time <= 0)
                 VictoryForProps();
@@ -179,9 +188,6 @@ public class GameManager : MonoBehaviour
     private void AssignRole(Player player)
     {
 
-        UnRegisterPlayer("Player "+player.id);
-        Destroy(player);    
-            
             
         int nbrhuntertot = 0;
         int nbrhunter=  Mathf.RoundToInt(players.Count * 3 / 10+1);
@@ -207,5 +213,8 @@ public class GameManager : MonoBehaviour
         }
          
             
+        UnRegisterPlayer("Player "+player.id);
+        Destroy(player);    
+
     }
 }
