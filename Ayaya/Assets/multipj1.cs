4e660b756b73132d4e98403d60e089262bc04028
SqueeBC <etiennesamatiallian@gmail.com>
2019-06-02 15:25:43 +0200

    desmodifs

--- Ayaya/Assets/multipj1.cs
+++ Ayaya/Assets/multipj1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public class multipj1 : MonoBehaviour
@@ -8,11 +9,17 @@ public class multipj1 : MonoBehaviour
     public GameObject spawnPoint9;
     public GameObject j1;
     public GameObject actu;
+    private bool ok = false;
     
     // Start is called before the first frame update
     void Start()
     {
-        actu = j1;
-        actu.transform.position = spawnPoint9.transform.position;
+        if (!ok)
+        {
+            NetworkServer.Spawn(j1);
+            actu = j1;
+            actu.transform.position = spawnPoint9.transform.position;
+        }
+        ok = true;
     }
 }
