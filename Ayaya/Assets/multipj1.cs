735dac33975f3cc29ac650f75bac123fa3d3995c
SqueeBC <etiennesamatiallian@gmail.com>
2019-06-02 14:54:04 +0200

    push
     cxx

---
+++ Ayaya/Assets/multipj1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class multipj1 : MonoBehaviour
+{
+    public GameObject spawnPoint9;
+    public GameObject j1;
+    public GameObject actu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        actu = j1;
+        actu.transform.position = spawnPoint9.transform.position;
+    }
+}
