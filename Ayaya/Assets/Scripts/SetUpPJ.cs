7a5b1dc3da5bf54e342e6b0ee2ccd2e4a9aa3065
SqueeBC <etiennesamatiallian@gmail.com>
2019-06-02 17:43:13 +0200

    MULTI QUI MARCHE ,

--- Ayaya/Assets/Scripts/SetUpPJ.cs
+++ Ayaya/Assets/Scripts/SetUpPJ.cs
@@ -6,25 +6,54 @@ using UnityEngine.Networking;
 public class SetUpPJ : NetworkBehaviour
 {
     public Behaviour[] compoToDisable;
+    public bool manualSpawn = false;
     Camera cam;
 
     //inutile donc indispansable (permet à la classe d'exister mais fait rien...)
-    public SetUpPJ() { }
+    public SetUpPJ(){}
 
-    private void Start()
+    public void Start()
     {
-        if (!isLocalPlayer)
+        
+        if (manualSpawn)
         {
-            for (int i = 0; i < compoToDisable.Length; i++)
-            {
-                compoToDisable[i].enabled = false;
-            }
+            Spawn();
         }
         else
         {
-            cam = Camera.main;
-            if (cam != null)
-                cam.gameObject.SetActive(false);
+            if (!isLocalPlayer)
+            {
+                for (int i = 0; i < compoToDisable.Length; i++)
+                {
+                    compoToDisable[i].enabled = false;
+                }
+            }
+            else
+            {
+                cam = Camera.main;
+                if (cam != null)
+                    cam.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void Spawn()
+    {
+        if (gameObject.transform.GetComponent<Hunter>() != null)
+        {
+            gameObject.transform.GetChild(1).GetComponent<PlayerMotor>().enabled = true;
+            gameObject.transform.GetChild(1).GetComponent<PlayerControler>().enabled = true;
+            gameObject.transform.GetChild(1).GetChild(0).GetComponent<Camera>().enabled = true;
+            gameObject.transform.GetChild(1).GetChild(0).GetComponent<AudioListener>().enabled = true;
+            Debug.Log("coucou t'es un hunter et tu existe");
+        }
+        else
+        {
+            gameObject.transform.GetChild(0).GetComponent<PlayerMotor>().enabled = true;
+            gameObject.transform.GetChild(0).GetComponent<PlayerControler>().enabled = true;
+            gameObject.transform.GetChild(0).GetChild(0).GetComponent<Camera>().enabled = true;
+            gameObject.transform.GetChild(0).GetChild(0).GetComponent<AudioListener>().enabled = true;
+            Debug.Log("coucou t'es un prop et tu existe");
         }
     }
 
--- Ayaya/Assets/multipj1.cs
+++ Ayaya/Assets/multipj1.cs
@@ -6,19 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class multipj1 : MonoBehaviour
 {
-    public GameObject spawnPoint9;
+    public GameObject spawnPoint10;
     public GameObject j1;
-    public GameObject actu;
     private bool ok = false;
+    private Camera cam;
     
     // Start is called before the first frame update
     void Start()
     {
         if (!ok)
         {
-            NetworkServer.Spawn(j1);
-            actu = j1;
-            actu.transform.position = spawnPoint9.transform.position;
+            GameObject joueur = (GameObject) Instantiate(j1, spawnPoint10.transform.position, Quaternion.Euler(0.0f,0.0f,0.0f));
+            joueur.GetComponent<SetUpPJ>().manualSpawn = true;
+            NetworkServer.Spawn(joueur);
+            Debug.Log("azerty tu existeeeee");
+            cam = Camera.main;
+            if (cam != null)
+                cam.gameObject.SetActive(false);
         }
         ok = true;
     }
