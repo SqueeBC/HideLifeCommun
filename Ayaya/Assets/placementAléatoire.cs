84184a947f991c94364942e4e9db65d628bad9a0
SqueeBC <etiennesamatiallian@gmail.com>
2019-06-03 14:13:36 +0200

    mathieu a fait des choses

---
+++ Ayaya/Assets/placementAléatoire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class placementAléatoire : NetworkBehaviour
+{
+    public GameObject obj1;
+    public Transform pos1;
+    
+    
+    // Start is called before the first frame update
+    [Server]
+    void Start()
+    {
+        GameObject testobj1 = (GameObject) Instantiate(obj1,pos1);
+        NetworkServer.Spawn(testobj1);
+        Debug.Log("coiuytfdsq");
+    }
+}
